5b46c3ef17205561c095a2bd20b7b573d9175405
Ethan Glasser-Camp <ethan@betacantrips.com>
2016-01-18 22:56:03 -0500

    Requests: introduce newOrder

--- src/Network/Stockfighter/Requests.hs
+++ src/Network/Stockfighter/Requests.hs
@@ -2,11 +2,16 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Network.Stockfighter.Requests (
     heartbeat,
+    newOrder
     ) where
 
 import Control.Lens ((&), (.~), (^.))
+import Data.Aeson (encode)
+import qualified Data.Text as T
 import Network.Stockfighter.Responses (HeartbeatResponse)
-import Network.Stockfighter.Types (StockfighterEnvironment(SE, apiKey, session))
+import Network.Stockfighter.Types (Order,
+    RequestOrder(RequestOrder, roStock, roVenue),
+    StockfighterEnvironment(SE, apiKey, session))
 import Network.Wreq (Options, asJSON, defaults, header, responseBody)
 import qualified Network.Wreq.Session as S
 
@@ -18,4 +23,17 @@ heartbeat :: StockfighterEnvironment -> IO HeartbeatResponse
 heartbeat se@SE{session} = do
     let opts = stockfighterOptions se
     response <- S.getWith opts session "https://api.stockfighter.io/ob/api/heartbeat" >>= asJSON
+    return $ response ^. responseBody
+
+newOrder :: RequestOrder -> StockfighterEnvironment -> IO Order
+newOrder o@RequestOrder{roVenue, roStock} se@SE {session} = do
+    let opts = stockfighterOptions se
+        url = concat [
+            "https://api.stockfighter.io/ob/api/venues/",
+            T.unpack roVenue,
+            "/stocks/",
+            T.unpack roStock,
+            "/orders"
+            ]
+    response <- S.postWith opts session url (encode o) >>= asJSON
     return $ response ^. responseBody
--- src/Network/Stockfighter/Types.hs
+++ src/Network/Stockfighter/Types.hs
@@ -1,16 +1,123 @@
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Network.Stockfighter.Types (
     ApiKey,
+    Direction(..),
+    Order(..),
+    OrderType(..),
+    RequestOrder(..),
     -- FIXME: maybe don't export the constructor
     StockfighterEnvironment(..),
     ) where
 
+import Control.Applicative ((<$>), (<*>))
+import Control.Monad (unless)
+import Data.Aeson (FromJSON(parseJSON), ToJSON(toJSON), Value(String),
+                   (.=), (.:), object, withObject)
 import Data.ByteString (ByteString)
+import Data.Time.Clock (UTCTime)
+import Data.Text (Text)
 import qualified Network.Wreq.Session as S
 
 type ApiKey = ByteString
+
+type Money = Int  -- low 2 digits are cents
 
 -- | A handle on a Stockfighter instance.
 data StockfighterEnvironment = SE {
     apiKey :: ApiKey,
     session :: S.Session
     }
+
+data RequestOrder = RequestOrder {
+    roAccount :: Text,
+    roVenue :: Text,
+    roStock :: Text,
+    roPrice :: Maybe Money,  -- can be omitted for Market orders
+    roQuantity :: Int,
+    roDirection :: Direction,
+    roOrderType :: OrderType
+    }
+
+instance ToJSON RequestOrder where
+    toJSON RequestOrder {..} = object [
+        "account" .= roAccount,
+        "venue" .= roVenue,
+        "stock" .= roStock,
+        "price" .= roPrice,
+        "qty" .= roQuantity,
+        "direction" .= roDirection,
+        "orderType" .= roOrderType
+        ]
+
+
+data Direction =
+    Sell | Buy
+    deriving (Show, Read, Eq)
+
+instance ToJSON Direction where
+    toJSON Sell = String "sell"
+    toJSON Buy = String "buy"
+
+instance FromJSON Direction where
+    parseJSON (String "sell") = return Sell
+    parseJSON (String "buy") = return Buy
+    parseJSON x = fail $ "couldn't parse Direction from " ++ show x
+
+
+data OrderType =
+    Limit |
+    Market |
+    FillOrKill |
+    ImmediateOrCancel
+    deriving (Show, Read, Eq)
+
+instance ToJSON OrderType where
+    toJSON Limit = String "limit"
+    toJSON Market = String "market"
+    toJSON FillOrKill = String "fill-or-kill"
+    toJSON ImmediateOrCancel = String "immediate-or-cancel"
+
+instance FromJSON OrderType where
+    parseJSON (String "limit") = return Limit
+    parseJSON (String "market") = return Market
+    parseJSON (String "fill-or-kill") = return FillOrKill
+    parseJSON (String "immediate-or-cancel") = return ImmediateOrCancel
+    parseJSON x = fail $ "couldn't parse OrderType from " ++ show x
+
+
+data Order = Order {
+    oSymbol :: Text,
+    oVenue :: Text,
+    oDirection :: Direction,
+    oOriginalQuantity :: Int,
+    oQuantity :: Int,
+    oPrice :: Money,
+    oOrderType :: OrderType,
+    oId :: Integer,
+    oAccount :: Text,
+    oTimestamp :: UTCTime,
+    -- FIXME:
+    --oFills
+    oTotalFilled :: Int, -- presumably this matches oFills
+    oOpen :: Bool
+    } deriving (Show)
+
+instance FromJSON Order where
+    parseJSON = withObject "Order" $ \o -> do
+        isOK <- o .: "ok"
+        unless isOK $
+            fail $ "parsing order failed -- source was " ++ show o
+        Order <$> o .: "symbol"
+              <*> o .: "venue"
+              <*> o .: "direction"
+              <*> o .: "originalQty"
+              <*> o .: "qty"
+              <*> o .: "price"
+              <*> o .: "orderType"
+              <*> o .: "id"
+              <*> o .: "account"
+              <*> o .: "ts"
+              <*> o .: "totalFilled"
+              <*> o .: "open"
