da466a114eca5afe59bd4f6f6ab8644d6a2619e5
Ethan Glasser-Camp <ethan@betacantrips.com>
2016-04-16 16:05:00 -0400

    Lens all the things

--- src/Network/Stockfighter/Lens.hs
+++ src/Network/Stockfighter/Lens.hs
@@ -5,17 +5,28 @@
 {-# LANGUAGE TypeSynonymInstances #-}
 
 module Network.Stockfighter.Lens (
+    -- Order
     stock, venue, direction, originalQuantity,
     price, orderType, orderId, account, timestamp, totalFilled,
-    open
+    open,
+
+    -- Fill
+    quantity, --price, timestamp,
+
+    -- Quote
+    --stock, venue,
+    bid, ask, bidSize, askSize, bidDepth, askDepth,
+    lastPrice, lastQuantity, lastTimestamp, --timestamp,
     ) where
 
 import Control.Lens (Lens')
 import Control.Lens.TH (abbreviatedFields, makeLensesWith)
 import Network.Stockfighter.Types (
-    Order, OrderId)
+    Fill, Order, OrderId, Quote)
 
+makeLensesWith abbreviatedFields ''Fill
 makeLensesWith abbreviatedFields ''Order
+makeLensesWith abbreviatedFields ''Quote
 
 orderId :: HasId o OrderId => Lens' o OrderId
 orderId = Network.Stockfighter.Lens.id   -- oId abbreviates to id
